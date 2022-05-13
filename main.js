570bc267eab8a0733e777193c3e13555e8c5bf67
Sanvi <96112662+Bats34@users.noreply.github.com>
2022-05-13 15:08:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,118 @@
+Canvas= new fabric.Canvas("myCanvas");
+
+ball_y=0;
+ball_x=0;
+hole_y=400;
+hole_x=800;
+
+
+block_image_width = 5;
+block_image_height = 5;
+
+function load_img(){
+	fabric.Image.fromURL("golf-h.png", function(Img){
+		hole_obj=Img;
+		hole_obj.scaleToWidth(50);
+		hole_obj.scaleToHeight(50);
+		hole_obj.set({
+			top:hole_y,
+			left:hole_x
+		});
+		Canvas.add(hole_obj);
+	});
+	new_image();
+}
+
+function new_image()
+{
+	fabric.Image.fromURL("ball.png", function(Img){
+		ball_obj=Img;
+		ball_obj.scaleToWidth(50);
+		ball_obj.scaleToHeight(50);
+		ball_obj.set({
+			top:ball_y,
+			left:ball_x
+		});
+		Canvas.add(ball_obj);
+	});
+	
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	if((ball_x==hole_x)&&(ball_y==hole_y)) {
+		Canvas.remove(ball_obj);
+		document.getElementById("hd3").innerHTML="You scored the goal!!!";
+		document.getElementById("myCanvas").style.borderColor=red;
+	}
+	
+	else {
+		if(keyPressed == '38')
+		{
+			up();
+			console.log("up");
+		}
+		if(keyPressed == '40')
+		{
+			down();
+			console.log("down");
+		}
+		if(keyPressed == '37')
+		{
+			left();
+			console.log("left");
+		}
+		if(keyPressed == '39')
+		{
+			right();
+			console.log("right");
+		}
+	}
+	}
+	
+	function up()
+	{
+		if(ball_y> 0) {
+			ball_y=ball_y-10;
+			console.log("Up arrow key pressed, X="+ ball_x+"Y="+ball_y);
+			Canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function down()
+	{
+		if(ball_y< 550) {
+			ball_y=ball_y+10;
+			console.log("Down arrow key pressed, X="+ ball_x+"Y="+ball_y);
+			Canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function left()
+	{
+		if(ball_x>0) {
+			ball_x=ball_x-10;
+			console.log("Left arrow key pressed, X="+ ball_x+"Y="+ball_y);
+			Canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+
+	function right()
+	{
+		if(ball_x<950) {
+			ball_x=ball_x+10;
+			console.log("Right arrow key pressed, X="+ ball_x+"Y="+ball_y);
+			Canvas.remove(ball_obj);
+			new_image();
+		}
+	}
+	
+}
+
+
